22a15494053712eb0a9331ec46296d74931db537
Magdy <43680841+tw1tch-dev@users.noreply.github.com>
2024-05-01 01:57:31 +0300

    everything works fine

--- demo.py
+++ demo.py
@@ -6,6 +6,15 @@ from compression import Huffman
 from compression import Golomb
 from compression import Arithmetic
 
+with open('css/styles.css', 'r') as file:
+    css = file.read()
+
+st.markdown(f"""
+    <style>
+        {css}
+    </style>
+""", unsafe_allow_html=True)
+
 # title
 st.title("Data Compression Project")
 
@@ -13,9 +22,10 @@ st.title("Data Compression Project")
 with st.popover("Usage instructions"):
     st.markdown("Hello there! 👋  \n\nIn order to use Golomb Code, you need to put digits only and separated by commas (,).     \n Example: 2,5,1500,3,3,2")
 
+# text to encode
 text = st.text_input('Text to encode', 'abbfcsdfdddfadfafafa')
 
-golombOnly = False
+golombOnly = False # check whether to use golomb only or everything else
 if not text:
     st.warning("Text input is empty. Please enter text to use the encoding algorithms.")
     st.stop()
@@ -23,20 +33,19 @@ elif text.replace(',', '').isdigit():
     golombOnly = True
     parts = text.split(',')
 
-    all_digits = all(part.isdigit() for part in parts)
-
-    if all_digits:
-        st.write("All parts are digits separated by commas.")
-    else:
-        st.write("Input contains non-digit characters or improper comma separation.")
-else:
-    st.write("Input contains characters other than digits.")
-
 no_bitsBefore = len(text) * 8
 st.write('Bits before encoding: ', no_bitsBefore)
 H, prob = Metrics.entropy(text)
 st.write('Entropy: ', H)
-st.write('Probability occurence:', prob) # probability occurence
+# st.write('Probability occurence:', prob) # probability occurence
+
+formatted_string = "| Character | Probability |\n| ----------- | ----------- |\n"
+
+for letter, probability in prob.items():
+    formatted_string += f"| {letter} | {round(probability, 2)} |\n"
+
+st.markdown(formatted_string)
+st.markdown("----")
 
 def printResults(before, after, encoded, isArithmetic = False):
     if isArithmetic:
@@ -82,65 +91,71 @@ def printResults(before, after, encoded, isArithmetic = False):
         st.latex(latex_fraction)
         st.latex(latex_expression)
 
-# LZW
-st.header('LZW')
-lzw = LZW()
-encoded_file = lzw.LZW_encoder(text)
-bins = Metrics.binarify(encoded_file)
-l_avg = Metrics.Avg_length(bins, [1/len(bins)]*len(bins))
-before,after = Metrics.No_bits(text,bits_array=bins)
-printResults(before, after, encoded_file)
-st.write('Encoded file:', encoded_file)
-st.write('bins: ', bins)
-st.write('Average length: ', l_avg)
-efficiency = H/l_avg * 100
-f_efficiency = "{:.2f}".format(efficiency)
-st.write('Efficiency: ', f_efficiency, '%')
+if not golombOnly:
+    # LZW
+    st.header('LZW')
+    lzw = LZW()
+    encoded_file = lzw.LZW_encoder(text)
+    bins = Metrics.binarify(encoded_file)
+    l_avg = Metrics.Avg_length(bins, [1/len(bins)]*len(bins))
+    before,after = Metrics.No_bits(text,bits_array=bins)
+    printResults(before, after, encoded_file)
+    st.write('Average length: ', l_avg)
+    efficiency = H/l_avg * 100
+    f_efficiency = "{:.2f}".format(efficiency)
+    st.write('Efficiency: ', f_efficiency, '%')
+    st.markdown("----")
 
-# Huffman
-st.header("Huffman")
-huffman = Huffman()
-encoded, d = huffman.encode(text)
-after = len(encoded)
-printResults(no_bitsBefore, after, encoded)
-formatted_string = "| Letter | Codeword |\n| ----------- | ----------- |\n"
+    # Huffman
+    st.header("Huffman")
+    huffman = Huffman()
+    encoded, d = huffman.encode(text)
+    after = len(encoded)
+    printResults(no_bitsBefore, after, encoded)
+    formatted_string = "| Letter | Codeword |\n| ----------- | ----------- |\n"
 
-for letter, codeword in d.items():
-    formatted_string += f"| {letter} | {codeword} |\n"
+    for letter, codeword in d.items():
+        formatted_string += f"| {letter} | {codeword} |\n"
 
-st.markdown(formatted_string)
+    st.markdown(formatted_string)
 
-avg_length = 0
-for char, code in d.items():
-    p = prob[char]
-    avg_length += len(code) * p
+    avg_length = 0
+    for char, code in d.items():
+        p = prob[char]
+        avg_length += len(code) * p
 
-st.write("Average length:", avg_length)
-efficiency = H/avg_length * 100
-f_efficiency = "{:.2f}".format(efficiency)
-st.write('Efficiency: ', f_efficiency, '%')
+    st.write("Average length:", avg_length)
+    efficiency = H/avg_length * 100
+    f_efficiency = "{:.2f}".format(efficiency)
+    st.write('Efficiency: ', f_efficiency, '%')
+    st.markdown("----")
 
 
-# RLE
-st.header('RLE')
-rle = RLE()
-encoded_file = rle.run_length_encoding(text)
-before, after = Metrics.No_bits(text, encoded_file)
-printResults(before, after, encoded_file)
+    # RLE
+    st.header('RLE')
+    rle = RLE()
+    encoded_file = rle.run_length_encoding(text)
+    before, after = Metrics.No_bits(text, encoded_file)
+    printResults(before, after, encoded_file)
+    st.markdown("----")
 
-# Arithmetic
-st.header('Arithmetic')
-H, table = Metrics.entropy(text)
-symbols = list(table.keys())
-probabilities = [round(prob, 2) for prob in table.values()]
-sorted_symbols, sorted_probabilities = zip(*sorted(zip(symbols, probabilities)))
-sequence = st.text_input('Sequence', 'abc') # needs revision
-encoded_value = Arithmetic.encode_sequence(sequence, sorted_symbols, sorted_probabilities)      
+    # Arithmetic
+    st.header('Arithmetic')
+    H, table = Metrics.entropy(text)
+    symbols = list(table.keys())
+    probabilities = [round(prob, 2) for prob in table.values()]
+    sorted_symbols, sorted_probabilities = zip(*sorted(zip(symbols, probabilities)))
+    sequence = st.text_input('Sequence') # needs revision
+    if not sequence:
+        st.warning("You need to put in the sequence.")
+        st.stop()
+    encoded_value = Arithmetic.encode_sequence(sequence, sorted_symbols, sorted_probabilities)      
 
-binary_encoded_value = bin(int(encoded_value * (2 ** 64)))[2:]
-num_bits_after = len(binary_encoded_value)
+    binary_encoded_value = bin(int(encoded_value * (2 ** 64)))[2:]
+    num_bits_after = len(binary_encoded_value)
 
-printResults(no_bitsBefore, num_bits_after, encoded_value, isArithmetic = True)
+    printResults(no_bitsBefore, num_bits_after, encoded_value, isArithmetic = True)
+    st.markdown("----")
 
 # GOLOMB ONLY WORKS WITH INTEGERS
 if golombOnly:
@@ -150,7 +165,6 @@ if golombOnly:
     num_list = []
     m = 1000
     parts = text.split(',')
-    # Convert parts to integers and find the maximum number
     for part in parts:
         num = int(part)
         num_list.append(num)
@@ -158,10 +172,6 @@ if golombOnly:
     if num_list:
         max_num = max(num_list)
         m = 2 ** (max_num.bit_length())
-
-        # # Encode each number using Golomb encoding
-        # for num in num_list:
-        #     encoded_file.append(golomb.golomb_encode(num, m))
 
     m_input = st.text_input('M value', m)
     m = int(m_input)
