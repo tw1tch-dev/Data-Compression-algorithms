f9dca5ffb2ccecaa5c28dd801984b40384e6c6bf
Magdy <43680841+tw1tch-dev@users.noreply.github.com>
2024-04-30 15:46:58 +0300

    last commit for golomb

--- app.py
+++ app.py
@@ -102,16 +102,15 @@ col4, col5 = st.columns(2)
     
 with col4:
     st.header("Golomb")
+    
     golomb = Golomb()
     encoded_file = []
     m = 1000
     for integer in text:
         encoded_file.append(Golomb.golomb_encode(ord(integer),m))
-    # st.write(encoded_file)
-
-    # num_bits = len(encoded_file)
+    
     n_bits_before, n_bits_after = Metrics.No_bits(text, None, encoded_file)
-    printResults(no_bitsBefore, num_bits)
+    printResults(n_bits_before, n_bits_after)
 
 with col5:
     st.header("LZW")
